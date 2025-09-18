e76fcf595125025d9a73aee5696186e5d0fa6ccd
Sophia09-pixel <68878547+Sophia09-pixel@users.noreply.github.com>
2025-09-17 21:00:42 -0300

    Delete slnPerfomance directory

--- slnPerfomance/perfomance-cache/Model/Users.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace perfomance_cache.Model
-{
-    public class Users
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
-}
